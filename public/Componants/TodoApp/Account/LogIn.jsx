fda59dc25c5ac7f2eb8a4f1d8206803d676a4ec7
ABN HALLOMAH <alhamawymohamed@gmail.com>
2026-03-14 17:28:03 +0300

    uploade 6

---
+++ public/Componants/TodoApp/Account/LogIn.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function LogIn() {
+  return (
+    <div>
+      <h2>Log In</h2>
+      <p>Log in form goes here.</p>
+    </div>
+  )
+}
---
+++ public/Componants/TodoApp/Account/SignUp.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function SignUp() {
+  return (
+    <div>
+      <h2>Sign Up</h2>
+      <p>Sign up form goes here.</p>
+    </div>
+  )
+}
